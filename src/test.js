71d7d8b427f271f9dca45ecc6c8288655c1666dd
AzerQ <46249148+AzerQ@users.noreply.github.com>
2022-09-28 22:14:46 +0300

    [Hour1]

--- src/test.js
+++ src/test.js
@@ -1,16 +1,19 @@
-export function T1(){
+export function T1() {
     console.log(1);
 }
-export function T2(){
+
+export function T2() {
     console.log(2);
 }
-export function T3(){
+
+export function T3() {
     console.log(3);
 }
-export function Greeting(Name){
+
+export function Greeting(Name) {
     console.log(`Hello, ${Name}`);
 }
 
-export function Add(a,b){
-    return a+b;
+export function Add(a, b) {
+    return a + b;
 }
