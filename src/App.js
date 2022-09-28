d2121f13930e6268150fa3289f01557e3eadc556
AzerQ <46249148+AzerQ@users.noreply.github.com>
2022-09-28 22:48:06 +0300

    [Hour2]

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
+    const [likes, setLikes] = useState(0);
+    const [value, setValue] = useState("Какой-то текст");
+
+    function inc() {
+        setLikes(likes + 1);
+        console.log(`likes count = ${likes}`);
+    }
+
+    function dec() {
+        setLikes(likes - 1);
+        console.log(`likes count = ${likes}`);
+    }
+
     //Hi there
+
     return (
         <div className="App">
-            hi
+            <h1>{likes}</h1>
+            <h1>{value}</h1>
+            <input type="text" value={value}
+                   onChange={event => setValue(event.target.value) }
+            />
+            <button onClick={inc}>inc</button>
+            <button onClick={dec}>dec</button>
         </div>
     );
 }
